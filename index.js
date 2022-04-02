d6f2cf3546fdfdb2f47f05af10257077c6dee417
Yasikhorram <khorramyasaman@gmail.com>
2022-04-02 11:56:52 -0500

    add html file

---
+++ index.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="description"
+      content="This app helps you to jumpstart your day right!"
+    />
+    <meta name="keywords" content="youtube, motivation, happiness, life, positive" />
+
+    <title>Motivation Me</title>
+
+    <!-- external CSS link -->
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <h1></h1>
+    <img src="img/chair.png" alt="generic youtube gaming chair" />
+
+    <h2>Remote</h2>
+    <ul>
+      <li id="job"></li>
+      <li id="family"></li>
+      <li id="blue"></li>
+      <li id="yellow"></li>
+    </ul>
+
+    <script type="text/javascript" src="js/main.js"></script>
+  </body>
+</html>
